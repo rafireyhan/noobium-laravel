318ea43fc98c69b0be90a99dcdacd63d8d7c00e3
rafireyhan <haikalmoon123@yahoo.co.id>
2023-11-07 19:25:58 +0700

    Inisiasi Awal

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'name' => 'Startup',
+                'slug' => 'startup',
+            ],
+            [
+                'name' => 'Life',
+                'slug' => 'life',
+            ],
+            [
+                'name' => 'Life Lessons',
+                'slug' => 'life-lessons',
+            ],
+            [
+                'name' => 'Politics',
+                'slug' => 'politics',
+            ],
+            [
+                'name' => 'Travel',
+                'slug' => 'travel',
+            ],
+            [
+                'name' => 'Poetry',
+                'slug' => 'poetry',
+            ],
+            [
+                'name' => 'Entreprenuership',
+                'slug' => 'entreprenuership',
+            ],
+            [
+                'name' => 'Education',
+                'slug' => 'education',
+            ],
+            [
+                'name' => 'Health',
+                'slug' => 'health',
+            ],
+            [
+                'name' => 'Love',
+                'slug' => 'love',
+            ],
+            [
+                'name' => 'Design',
+                'slug' => 'design',
+            ],
+            [
+                'name' => 'Writing',
+                'slug' => 'writing',
+            ],
+            [
+                'name' => 'Technology',
+                'slug' => 'Technology',
+            ],
+        ]);
+    }
+}
